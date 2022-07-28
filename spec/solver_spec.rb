8b1453a3d2a95448208db9940b9671a4cf2b8be0
rolandfbk <rolandfbk@gmail.com>
2022-07-28 16:23:23 +0200

    grouped all factorial test in one context

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,27 +1,30 @@
 require './solver'
 
 describe Solver do
-  it "The factorial method returns the factorial value of a number greater than 0" do
-    result = Solver.new.factorial 5
+  context "Factorial testing" do
+    it "The factorial method returns the factorial value of a number greater than 0" do
+      result = Solver.new.factorial 5
 
-    expect(result).to eq 120
+      expect(result).to eq 120
+    end
+
+    it "The factorial method returns the factorial value of a number equal to 0" do
+      result = Solver.new.factorial 0
+
+      expect(result).to eq 1
+    end
+
+    it "The factorial method returns the factorial value of a number equal to 1" do
+      result = Solver.new.factorial 1
+
+      expect(result).to eq 1
+    end
+
+    it "The factorial method returns the factorial value of a number less than 0" do
+      result = Solver.new.factorial -5
+
+      expect(result).to eq 'The number must be greater or equal to 0!'
+    end
   end
-
-  it "The factorial method returns the factorial value of a number equal to 0" do
-    result = Solver.new.factorial 0
-
-    expect(result).to eq 1
-  end
-
-  it "The factorial method returns the factorial value of a number equal to 1" do
-    result = Solver.new.factorial 1
-
-    expect(result).to eq 1
-  end
-
-  it "The factorial method returns the factorial value of a number less than 0" do
-    result = Solver.new.factorial -5
-
-    expect(result).to eq 'The number must be greater or equal to 0!'
-  end
+  
 end
